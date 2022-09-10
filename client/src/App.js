8394fb811e189399531480f2ea0c65f1f8f07bc2
dimmig <galagan.dm@gmail.com>
2022-09-10 15:59:49 +0300

    deleted default icon

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { HashRouter as Router } from "react-router-dom";
 import { Switcher } from "./components/Switcher";
 
-
 function App() {
   return (
     <Router>
